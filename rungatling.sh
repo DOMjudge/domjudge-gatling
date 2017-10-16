84cdcdb90324563ca142fa883dac4faa1112512b
Keith Johnson <kj@ubergeek42.com>
2017-10-15 20:57:10 -0400

    Make the rungatling script take an argument to the server

--- rungatling.sh
+++ rungatling.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 GATLING=$DIR/gatling-charts-highcharts-bundle-2.3.0/bin/gatling.sh
 
+URL=${1:-http://localhost/domjudge}
+echo "Running gatling against: $URL"
 
-export JAVA_OPTS="-Dbaseurl=http://localhost/domjudge"
+export JAVA_OPTS="-Dbaseurl=$URL"
 $GATLING --results-folder $PWD/reports --simulations-folder $PWD/simulations --bodies-folder $PWD/bodies --mute  --simulation domjudge.AdminSetup
-$GATLING --results-folder $PWD/reports --simulations-folder $PWD/simulations --bodies-folder $PWD/bodies --mute  --simulation domjudge.ContestSimulation -rd "'$1'"
+$GATLING --results-folder $PWD/reports --simulations-folder $PWD/simulations --bodies-folder $PWD/bodies --mute  --simulation domjudge.ContestSimulation
