31ddde8c3320b7c13a7641cd529e3f4b2dfa8951
Keith Johnson <kj@ubergeek42.com>
2017-10-15 20:42:17 -0400

    Look for gatling in the current directory

--- rungatling.sh
+++ rungatling.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-GATLING=~/temp/gatling-charts-highcharts-bundle-2.1.7/bin/gatling.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+GATLING=$DIR/gatling-charts-highcharts-bundle-2.3.0/bin/gatling.sh
 
 export JAVA_OPTS="-Dbaseurl=http://example.com/domjudge/"
 $GATLING --results-folder $PWD/reports --simulations-folder $PWD/simulations --bodies-folder $PWD/bodies --mute  --simulation domjudge.AdminSetup
