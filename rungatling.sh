9aa9ace0e20ed9ab9be75fd57ea93f13745e6c7c
Keith Johnson <kj@ubergeek42.com>
2017-10-15 20:42:54 -0400

    Assume domjudge is running locally

--- rungatling.sh
+++ rungatling.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 GATLING=$DIR/gatling-charts-highcharts-bundle-2.3.0/bin/gatling.sh
 
-export JAVA_OPTS="-Dbaseurl=http://example.com/domjudge/"
+
+export JAVA_OPTS="-Dbaseurl=http://localhost/domjudge"
 $GATLING --results-folder $PWD/reports --simulations-folder $PWD/simulations --bodies-folder $PWD/bodies --mute  --simulation domjudge.AdminSetup
 $GATLING --results-folder $PWD/reports --simulations-folder $PWD/simulations --bodies-folder $PWD/bodies --mute  --simulation domjudge.ContestSimulation -rd "'$1'"
