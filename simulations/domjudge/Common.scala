b6116812df0ed05d0918b5dee3c54f051b3bd356
Keith Johnson <kj@ubergeek42.com>
2020-11-08 02:23:17 +0000

    Update gatling simulation to work with new domjudge

--- simulations/domjudge/Common.scala
+++ simulations/domjudge/Common.scala
@@ -42,15 +42,18 @@ object User {
         http("Registration page get csrf")
         .get("/register")
         .check(
-          regex("""<input type="hidden" id="user_registration__token" name="user_registration._token." value="([^"]*)" />""")
+          regex("""<input type="hidden" id="user_registration__token" name="user_registration\[_token\]" value="([^"]*)" """)
           .find
           .saveAs("csrftoken")
         ))
       .exec(http("Register user")
         .post("/register")
         .formParam("user_registration[username]", session => user(session))
+        .formParam("user_registration[name]", session => user(session)) // todo make this say "Gatling ###"
+        .formParam("user_registration[teamName]", session => user(session))
         .formParam("user_registration[plainPassword][first]", session => realpass(session))
         .formParam("user_registration[plainPassword][second]", session => realpass(session))
+        .formParam("user_registration[existingAffiliation]", "1")
         .formParam("user_registration[_token]", "${csrftoken}")
       )
   }
@@ -58,54 +61,81 @@ object User {
 
 object Team {
   def _submit(langid: String, filename: String) =
-    exec(http("Submit Solution")
-      .post("/team/upload.php")
-      .formParam("probid","1")
-      .formParam("langid", langid)
-      .formParam("submit", "submit")
-      .formUpload("code[]", filename))
+    exec(
+      http("Get submit solution form")
+      .get("/team/submit")
+      .check(
+        regex("""<input type="hidden" id="submit_problem__token" name="submit_problem\[_token\]" value="([^"]*)"""").find
+        .saveAs("csrftoken")
+        ,
+        // get and save the problem id for the "hello" problem
+        regex("""<option value="([^"]*)">hello""")
+        .saveAs("problem_id")
+    ))
+    .exec(http("Submit Solution ${langid}")
+      .post("/team/submit")
+      .formParam("submit_problem[_token]","${csrftoken}")
+      .formParam("submit_problem[problem]","${problem_id}")
+      .formParam("submit_problem[language]", langid)
+      .formUpload("submit_problem[code][]", filename)
+      .formParam("submit", ""))
   def _submit_with_entrypoint(langid: String, filename: String, entry_point: String) =
-    exec(http("Submit Solution")
-      .post("/team/upload.php")
-      .formParam("probid","1")
-      .formParam("langid", langid)
-      .formParam("submit", "submit")
-      .formParam("entry_point", entry_point)
-      .formUpload("code[]", filename))
+    exec(
+      http("Get submit solution form")
+      .get("/team/submit")
+      .check(
+        regex("""<input type="hidden" id="submit_problem__token" name="submit_problem\[_token\]" value="([^"]*)"""").find
+        .saveAs("csrftoken")
+        ,
+        // get and save the problem id for the "hello" problem
+        regex("""<option value="([^"]*)">hello""")
+        .saveAs("problem_id")
+    ))
+    .exec(http("Submit Solution ${langid}")
+      .post("/team/submit")
+      .formParam("submit_problem[_token]","${csrftoken}")
+      .formParam("submit_problem[problem]","${problem_id}")
+      .formParam("submit_problem[language]", langid)
+      .formParam("submit_problem[entry_point]", entry_point)
+      .formUpload("submit_problem[code][]", filename)
+      .formParam("submit", ""))
 
-  val submit_java   = exec(_submit("java",    "test-hello.java"))
-  val submit_c      = exec(_submit("c",       "test-hello.c"))
-  val submit_hs     = exec(_submit("hs",      "test-hello.hs"))
-  val submit_lua    = exec(_submit("lua",     "test-hello.lua"))
-  val submit_js     = exec(_submit("js",      "test-hello.js"))
-  val submit_csharp = exec(_submit("csharp",  "test-hello.cs"))
+  val submit_java      = exec(_submit("java",    "test-hello.java"))
+  val submit_c         = exec(_submit("c",       "test-hello.c"))
+  val submit_hs        = exec(_submit("hs",      "test-hello.hs"))
+  val submit_lua       = exec(_submit("lua",     "test-hello.lua"))
+  val submit_js        = exec(_submit("js",      "test-hello.js"))
+  val submit_csharp    = exec(_submit("csharp",  "test-hello.cs"))
+  val submit_py2       = exec(_submit("py2",     "test-hello.py2"))
+  val submit_py3       = exec(_submit("py3",     "test-hello.py3"))
   val submit_nonewline = exec(_submit("c",       "test-output-nonewline.c"))
-  val submit_kt     = exec(_submit_with_entrypoint("kt",  "test-hello.kt", "Test_helloKt"))
-  val submit_py2    = exec(_submit_with_entrypoint("py2", "test-hello.py2", "test-hello.py2"))
-  val submit_py3    = exec(_submit_with_entrypoint("py3", "test-hello.py3", "test-hello.py3"))
+  val submit_kt        = exec(_submit_with_entrypoint("kt",  "test-hello.kt", "Test_helloKt"))
   def requestclarification() =
-    exec(http("Request Clarification")
-      .post("/team/clarification.php")
-      .formParam("problem", "${cid}-general")
-      .formParam("bodytext", "${user} needs help")
-      .formParam("submit", "Send"))
+    exec(
+        http("Get request clarification form")
+        .get("/team/clarifications/add")
+        .check(
+          regex("""<input type="hidden" id="team_clarification__token" name="team_clarification\[_token\]" value="([^"]*)"""")
+          .saveAs("csrftoken")
+          ,
+          // get and save the clarification subject id for the "General issue" category
+          regex("""<option value="([^"]*-general)">General""").find.saveAs("clarification_subject")
+        ))
+    .exec(http("Request Clarification")
+      .post("/team/clarifications/add")
+      .formParam("team_clarification[recipient]", "dummy")
+      .formParam("team_clarification[subject]", "${clarification_subject}")
+      .formParam("team_clarification[message]", "${user} needs help")
+      .formParam("team_clarification[_token]", "${csrftoken}")
+      .formParam("submit", "")
+    )
 
   val teampage = exec(http("Team Page").get("/team/")
       .check(
-        regex("""<a href="team\.php.*?=(.*?)">""").find.saveAs("team_id")
+        regex("""(?s)<td class="scoretn.*" title="${user}">.*<a data-ajax-modal href="/team/team/(.*?)">""").find.saveAs("team_id")
       ))
-  val teampage_cid = exec(http("Team Page(for CID)").get("/team/")
-      .check(
-        regex("""<option value="([^"]*)".*>gatling""").find.saveAs("cid")
-      ))
-  // For debugging
-  //.exec(session => {
-  //      println(session("team_id"))
-  //      session
-  //    })
-  val teamdetails = exec(http("Team Details").get("/team/team.php?id=${team_id}"))
-  val teamscoreboard = exec(http("Team Scoreboard").get("/team/scoreboard.php"))
-  val teamclarifications = exec(http("View Clarifications").get("/team/clarification.php"))
+  val teamdetails = exec(http("Team Details").get("/team/team/${team_id}"))
+  val teamscoreboard = exec(http("Team Scoreboard").get("/team/scoreboard"))
 }
 
 object Spectator {
@@ -119,96 +149,7 @@ object Spectator {
 
 object Jury {
 
-  // Sets the session value "contest_id" with the resulting contest id(to be used when uploading a problem)
-  def create_contest(shortname: String = "test", name: String = "gatling test contest"): ChainBuilder = {
-    val contestformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  // TODO: add jury members browsing around/answering clarifications/etc
+  // def view_submissions = ...
 
-    return exec(http("Check if contest already exists")
-      .get("/jury/contests.php")
-      .check(
-        regex("""id=(.*?)">""" + shortname).optional.saveAs("contest_id")
-      )
-    )
-    .doIf(session => session("contest_id").asOption[Any].isDefined) {
-      exec(http("Delete existing contest")
-        .post("/jury/delete.php")
-        .formParam("table", "contest")
-        .formParam("cid", "${contest_id}")
-        .formParam("confirm", "Yes I'm sure!")
-      )
-    }
-    .exec(http("Create Contest")
-      .post("/jury/edit.php")
-      .formParam("data[0][shortname]", shortname)
-      .formParam("data[0][name]", name)
-      .formParam("data[0][activatetime_string]", "-00:00")
-      .formParam("data[0][starttime_string]", contestformat.format(Calendar.getInstance().getTime()) + " America/New_York")
-      .formParam("data[0][freezetime_string]", "+99:00")
-      .formParam("data[0][endtime_string]", "+99:00")
-      .formParam("data[0][unfreezetime_string]", "+99:00")
-      .formParam("data[0][deactivatetime_string]", "+99:00")
-      .formParam("data[0][process_balloons]", "1")
-      .formParam("data[0][public]", "1")
-      .formParam("data[0][mapping][1][items]", "")
-      .formParam("data[0][enabled]", "1")
-      .formParam("problems", "")
-      .formParam("data[0][mapping][0][items]", "")
-      .formParam("data[0][mapping][0][fk][0]", "cid")
-      .formParam("data[0][mapping][0][fk][1]", "probid")
-      .formParam("data[0][mapping][0][table]", "contestproblem")
-      .formParam("data[0][mapping][1][fk][0]", "cid")
-      .formParam("data[0][mapping][1][fk][1]", "teamid")
-      .formParam("data[0][mapping][1][table]", "contestteam")
-      .formParam("cmd", "add")
-      .formParam("table", "contest")
-      .formParam("referrer", "")
-    ).exec(http("Get Contest")
-      .get("/jury/contests.php")
-      .check(
-        regex("""id=(.*?)">""" + shortname).find.saveAs("contest_id")
-      )
-    )
-  }
-
-// call as one of the following
-// exec(Jury.upload_problem("hello-testcase.zip"))
-// exec(Jury.upload_problem("hello-testcase.zip", session => session("contest_id").as[String]))
-  def upload_problem(problem_archive: String, contest_id: Expression[String] = null): ChainBuilder = {
-    val default:Expression[String] = session => session("contest_id").as[String]
-    val cid = contest_id match {	case a:Expression[String] => a;	case _ => default }
-    return exec(http("Upload Problem")
-        .post("/jury/problem.php")
-        .formParam("contest", session => cid(session))
-        .formParam("upload", "Upload")
-        .formUpload("problem_archive[]", problem_archive))
-  }
-
-  // Takes in a Map("config_item_name" -> "config_value") of new configuration values to apply.
-  // All other options will remain unchanged
-  def modify_config(config: Map[String,String]) =
-    exec(http("Load Current Configuration")
-      .get("/jury/config.php")
-      .check(
-        regex("""<input.*name="config_(.*?)".*value="(.*?)".*>""").ofType[(String,String)].findAll.saveAs("current_config")
-      )
-    ).exec(http("Update Configuration")
-          .post("/jury/config.php")
-          .formParam("save", "Save")
-          .formParamMap(session => session("current_config").as[List[(String,String)]].toMap ++ config))
-
-  // Expects to be called like:
-  // Jury.enable_language("C#","csharp",)
-  def enable_language(lang: String, langid: String, extensions: List[String]) =
-		exec(http("Enable $langid")
-			.post("/jury/edit.php")
-			.formParam("keydata[0][langid]", langid)
-			.formParam("data[0][name]", lang)
-			.formParam("data[0][allow_submit]", "1")
-			.formParam("data[0][allow_judge]", "1")
-			.formParam("data[0][time_factor]", "1")
-			.formParam("data[0][compile_script]", langid)
-			.formParam("data[0][extensions]", "[\""+ extensions.mkString("\", \"") + "\"]")
-			.formParam("cmd", "edit")
-			.formParam("table", "language")
-			.formParam("referrer", "languages.php"))
 }
--- simulations/domjudge/ContestSimulation.scala
+++ simulations/domjudge/ContestSimulation.scala
@@ -11,17 +11,23 @@ import Team._
 import Spectator._
 
 class ContestSimulation extends Simulation {
-  var feeder = Iterator.from(0).map(i => Map("user" -> f"gatlinguser$i%04d"))
+  // Configure the base url, and other global options related to all requests
   val httpProtocol = http
     .baseUrl(System.getProperty("baseurl"))
+     // This makes it load dependent resources: things like images, javascript, css, etc
     .inferHtmlResources()
 
   // Scenario that just grabs the public scoreboard every 30 seconds, some number of times
-  // This will run for 10 minutes per "user"
-  val public_boards = scenario("PublicScoreboards").exec(Spectator.monitor_scoreboard(10))
+  // This will run for 10 minutes per "user". The scoreboard page autorefreshes every 30s,
+  // so this is a good approximation of their activity.
+  val spectatorScenario = scenario("PublicScoreboards").exec(Spectator.monitor_scoreboard(10))
 
-  // Takes around 3 minutes to run(and consists of some 36ish requests)
-  val scn = scenario("TeamExample")
+  // Generate an infinite number of users, starting from gatling000000
+  var feeder = Iterator.from(0).map(i => Map("user" -> f"gatling$i%05d"))
+
+  // This scenario pretends to be a team. It takes around 3 minutes, and makes ~250 requests(including dependent resources)
+  // It tries to be similar to what a team might do, load the team page, submit clarifications, submit problems, load the scoreboard, etc.
+  val teamScenario = scenario("TeamExample")
     .feed(feeder)
     // Load the public scoreboard, then register/login
     .exec(Spectator.getscoreboard).pause(1)
@@ -33,18 +39,19 @@ class ContestSimulation extends Simulation {
     .exitHereIfFailed
 
     // Make a submission, view team information, check scoreboard
-    .exec(Team.teampage_cid).pause(5) // Used to get the contestid(we'll need it later to request a clarification)
     .exec(Team.teampage).pause(5)
     .exec(Team.submit_java).pause(10)
     .exec(Team.teamscoreboard).pause(10)
+
+    // Do some more team like stuff
     .exec(Team.teamdetails).pause(5)
     .exec(Team.teampage).pause(7)
     .exec(Team.teamscoreboard).pause(9)
     .exec(Team.teampage).pause(3)
 
-    // Submit a clarification and then check it
+    // Submit a clarification and then "check" it
     .exec(Team.requestclarification).pause(6)
-    .exec(Team.teamclarifications).pause(12)
+    .exec(Team.teampage).pause(12)
 
     // Make some submissions, and check the team page periodically
     .exec(Team.teampage).pause(3)
@@ -62,38 +69,43 @@ class ContestSimulation extends Simulation {
     .exec(Team.submit_nonewline).pause(11)
     .exec(Team.teampage).pause(16)
     .exec(Team.teampage).pause(5)
-  setUp(scn.inject(
+
+  setUp(teamScenario.inject(
       atOnceUsers(1) // start with 1 users
-			//atOnceUsers(100) // start with 1 users
+			// atOnceUsers(100) // start with 100 users
             //constantUsersPerSec(5) during (30 seconds) randomized  // gets to ~150 users in 30seconds
       //      constantUsersPerSec(18) during (60 seconds) randomized  // gets to ~1000(ish) users in 60seconds
 			//rampUsers(500) over (60 seconds)
 
       // Use with the throttle commands below
       //constantUsersPerSec(30) during (45 seconds) // 1350 users?
-  ))
+  ), spectatorScenario.inject(atOnceUsers(1)))
     /*.throttle(
       jumpToRps(20).holdFor(2 minutes)
     )*/
     .maxDuration(6 minutes)
-    /*.throttle(
+    .throttle(
+      // reachRps(1) in (30 seconds),
+      // reachRps(5) in (30 seconds),
+      // reachRps(10) in (30 seconds),
+      // reachRps(25) in (30 seconds),
       reachRps(50) in (30 seconds),
       holdFor(1 minute),
       reachRps(100) in (30 seconds),
-      holdFor(1 minute),
-      reachRps(150) in (30 seconds),
-      holdFor(1 minute),
-      reachRps(200) in (30 seconds),
-      holdFor(2 minutes)*/
-      /*reachRps(250) in (30 seconds),
-      holdFor(1 minute),
-      reachRps(300) in (30 seconds),
-      holdFor(1 minute),
-      reachRps(400) in (30 seconds),
-      holdFor(1 minute),
-      reachRps(500) in (30 seconds),
-      holdFor(5 minutes)*/
-    //)
+      // holdFor(1 minute),
+      // reachRps(150) in (30 seconds),
+      // holdFor(1 minute),
+      // reachRps(200) in (30 seconds),
+      // holdFor(2 minutes)
+      // reachRps(250) in (30 seconds),
+      // holdFor(1 minute),
+      // reachRps(300) in (30 seconds),
+      // holdFor(1 minute),
+      // reachRps(400) in (30 seconds),
+      // holdFor(1 minute),
+      // reachRps(500) in (30 seconds),
+      // holdFor(5 minutes)
+    )
     .protocols(httpProtocol)
     .pauses(uniformPausesPlusOrMinusPercentage(25)) // adjust pauses by 25percent either way
 
